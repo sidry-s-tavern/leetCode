03c4921f886f1b583a6dd7cf02d7385d0e33edae
sidry-s-tavern <isgudoshnikov@gmail.com>
2025-01-29 16:19:14 +0400

    initial "14. Lowest Common Prefix".

---
+++ src/LowestCommonPrefix.java
@@ -0,0 +1,6 @@
+public class LowestCommonPrefix {
+    public String longestCommonPrefix(String[] strs) {
+
+        return null;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        RomanToInt r2i = new RomanToInt();
-        System.out.println(r2i.romanToInt("III"));
-        System.out.println(r2i.romanToInt("LVIII"));
-        System.out.println(r2i.romanToInt("MCMXCVIII"));
+        LowestCommonPrefix lcp = new LowestCommonPrefix();
+        String[] strs = new String[]{"flower","flow","flight"};
+        lcp.longestCommonPrefix(strs);
     }
 }
