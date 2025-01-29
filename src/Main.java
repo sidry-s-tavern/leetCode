04abc411fab3d3b2b5e7e970b5008a2dbd9502a0
sidry-s-tavern <isgudoshnikov@gmail.com>
2025-01-29 13:02:03 +0400

    Answer to "9. Palindrome Number"

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        Two_Sum ts = new Two_Sum();
-        int [] nums = new int []{3,2,4};
-        ts.printTwoSum(nums,6);
+        PalindromeNumber pn = new PalindromeNumber();
+        System.out.println(pn.isPalindromeTotString(121));
+        System.out.println(pn.isPalindromeTotString(1231));
+        System.out.println(pn.isPalindromeTotString(-121));
+        System.out.println(pn.isPalindromeTotString(10));
     }
 }
---
+++ src/PalindromeNumber.java
@@ -0,0 +1,13 @@
+public class PalindromeNumber {
+    public boolean isPalindrome(int x) {
+        String xString = Integer.toString(x);
+        for (int i = 0; i < xString.length(); i++) {
+            if(xString.charAt(i) != xString.charAt(xString.length()-i-1)) return false;
+        }
+        return true;
+    }
+    public String isPalindromeTotString(int x)
+    {
+        return "For int: "+x+" answer is "+isPalindrome(x);
+    }
+}
