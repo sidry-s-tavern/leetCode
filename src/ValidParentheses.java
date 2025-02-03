b1686bb469c414a31f887bd1999d73e8dc4d4d64
sidry-s-tavern <isgudoshnikov@gmail.com>
2025-02-03 15:31:07 +0400

    20. Valid Parentheses. some changes to loop request of foo

--- src/ValidParentheses.java
+++ src/ValidParentheses.java
@@ -16,19 +16,32 @@ public class ValidParentheses {
 //        System.out.print(s + " -> ");
         if (!initialTests(s)) return false;
         boolean res = false;
-        if (isPaired(s,'(')) {
-            System.out.println("isPaired (:"+isPaired(s,'('));
-            res = isValid(s, '(');
-        }
-        if (isPaired(s,'[')) {
-            System.out.println("isPaired [:"+isPaired(s,'['));
-            res = isValid(s, '[');
-        }
-        if (isPaired(s,'{')) {
-            System.out.println("isPaired {:"+isPaired(s,'{'));
-            res = isValid(s, '{');
-        }
+        res = foo(s);
         return res;
+    }
+
+    private boolean foo(String s) {
+        if (checkBrakets(s, '(')) return false;
+        if (checkBrakets(s, '{')) return false;
+//        if (checkBrakets(s, '[')) return false;
+        return true;
+    }
+
+    private boolean checkBrakets(String s, char c) {
+        System.out.println("checkBrakets with s:" + s + " & c:" + c);
+        if (s.isEmpty()) return true;
+        if (isExist(s, c)) {
+            if (isPaired(s, c)) {
+                if (isValid(s, c)) {
+                    System.out.println(s + " > clean > " + cleanString(s, c));
+                    checkBrakets(cleanString(s, c),c);
+                } else {
+                    System.out.println("isValid > false");
+                    return false;
+                }
+            } else return false;
+        }
+        return false;
     }
 
     public boolean initialTests(String s) {
@@ -38,7 +51,10 @@ public class ValidParentheses {
     }
 
     public boolean isValid(String s, char c) {
-        return (s.indexOf(c) - s.indexOf(getClosed(c)) % 2 != 0);
+        System.out.println("4 s:" + s + " & c:" + c + " isValid: " + ((s.indexOf(c) - s.indexOf(getClosed(c)) % 2 != 0) &&
+                ((s.indexOf(c) - s.indexOf(getClosed(c)) < 0))));
+        return ((s.indexOf(c) - s.indexOf(getClosed(c)) % 2 != 0) &&
+                ((s.indexOf(c) - s.indexOf(getClosed(c)) < 0)));
     }
 
     private static char getClosed(char c) {
@@ -63,10 +79,10 @@ public class ValidParentheses {
         };
     }
 
-    public String cleanString(String s, char c)
-    {
-        String res;
-        res = s.substring(0,s.indexOf(c));
-        return res;
+    public String cleanString(String s, char c) {
+        if (isExist(s, c)) return (s.substring(0, s.indexOf(c))
+                + s.substring(s.indexOf(c) + 1, s.indexOf(getClosed(c)))
+                + s.substring(s.indexOf(getClosed(c)) + 1));
+        return s;
     }
 }
