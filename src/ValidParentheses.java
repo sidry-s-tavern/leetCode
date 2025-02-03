b8282ed3f99f0c90fca108c2be289f6fd64fb2b9
sidry-s-tavern <isgudoshnikov@gmail.com>
2025-02-03 12:56:46 +0400

    20. Valid Parentheses. Some changes

--- src/ValidParentheses.java
+++ src/ValidParentheses.java
@@ -13,14 +13,54 @@
 */
 public class ValidParentheses {
     public boolean isValid(String s) {
-        System.out.println(s);
+        System.out.print(s+" -> ");
         if (!initialTests(s)) return false;
-        return true;
+        boolean res = false;
+        if (isExist(s, '(')) {
+            res = isValid(s,'(');
+        }if (isExist(s, '[')) {
+            res = isValid(s,'[');
+        }if (isExist(s, '{')) {
+            res = isValid(s,'{');
+        }
+        return res;
     }
 
     public boolean initialTests(String s) {
         if (s.isEmpty()) return false;
         if (s.length() % 2 != 0) return false;
         return (s.indexOf('(') != -1) || (s.indexOf(')') != -1) || (s.indexOf('[') != -1) || (s.indexOf(']') != -1) || (s.indexOf('{') != -1) || (s.indexOf('}') != -1);
+    }
+
+    public boolean isPaired(String s) {
+        if (s.isEmpty()) return true;
+        if (s.indexOf('(') != -1) if (s.indexOf(')') != -1)
+            isPaired(s.substring(0, s.indexOf('('))
+                    + s.substring(s.indexOf('(') + 1, s.indexOf(')'))
+                    + s.substring(s.indexOf(')') + 1));
+        return false;
+    }
+
+    public boolean isValid(String s, char c) {
+        char cClosed = 0;
+        switch (c) {
+            case '(': {
+                cClosed = ')';
+                break;
+            }
+            case '{': {
+                cClosed = '}';
+                break;
+            }
+            case '[': {
+                cClosed = ']';
+                break;
+            }
+        }
+        return (s.indexOf(c) - s.indexOf(cClosed) % 2 != 0);
+    }
+
+    public boolean isExist(String s, char c) {
+        return s.indexOf(c) != -1;
     }
 }
