3bf6f1de148c06832b7c81368203081ffed9cf5a
sidry-s-tavern <isgudoshnikov@gmail.com>
2025-02-10 11:12:06 +0400

    27. Remove Element. init commit

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        RemoveDuplicatesFromSortedArray removeDuplicatesFromSortedArray = new RemoveDuplicatesFromSortedArray();
-//        int[] nums = new int[]{1,1,2};
-//        int[] nums = new int[]{0,0,1,1,1,2,2,3,3,4};
-        int[] nums = new int[]{-1,0,0,0,0,3,3};
-        System.out.println(removeDuplicatesFromSortedArray.removeDuplicates(nums));
-        removeDuplicatesFromSortedArray.printArray(nums);
+
     }
 }
