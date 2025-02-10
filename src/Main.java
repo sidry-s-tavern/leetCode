f5e75f4ee1264e1c767f47c38e67798ffb60d69e
sidry-s-tavern <isgudoshnikov@gmail.com>
2025-02-10 17:33:50 +0400

    66. Plus One. 111 / 111. done

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         PlusOne plusOne = new PlusOne();
-        UsefullMethods.printIntArray(plusOne.plusOne(new int[]{9,8,7,6,5,4,3,2,1,0}));
+        UsefullMethods.printIntArray(plusOne.plusOne(new int[]{4,9,9,9}));
     }
 }
--- src/PlusOne.java
+++ src/PlusOne.java
@@ -1,25 +1,38 @@
+import java.util.Arrays;
+
 //[1,2,3] -> [1,2,4]
 //[4,3,2,1] -> [4,3,2,2]
 //[9] -> [1,0]
 public class PlusOne {
     public int[] plusOne(int[] digits) {
-        System.out.println(arrayOfIntToStr(digits));
-        long integer = Long.parseLong(arrayOfIntToStr(digits)) + 1;
-        digits = stringToArrayOfInt(Long.toString(integer));
+        int nines = countOfNinesAtEnd(digits);
+        if (nines != digits.length) {
+            digits[digits.length - nines - 1] += 1;
+            for (int i = digits.length - nines; i < digits.length; i++) {
+                digits[i] = 0;
+            }
+        } else {
+            digits = voulmeUpArrayOfInt(digits);
+            for (int i = 0; i < digits.length; i++) {
+                if (digits[i] == 9) digits[i] = 0;
+            }
+            digits[0]=1;
+        }
         return digits;
     }
 
-    public String arrayOfIntToStr(int[] arrayOfInt) {
-        StringBuilder temp = new StringBuilder();
-        for (int e : arrayOfInt) temp.append(e);
-        return temp.toString();
+    private int countOfNinesAtEnd(int[] digits) {
+        int nines = 0;
+        for (int i = digits.length - 1; i >= 0; i--) {
+            if (digits[i] == 9) {
+                nines++;
+            } else break;
+        }
+        return nines;
     }
 
-    public int[] stringToArrayOfInt(String str) {
-        int[] result = new int[str.length()];
-        for (int i = 0; i < str.length(); i++) {
-            result[i] = str.charAt(i)-48;
-        }
-        return result;
+    public int[] voulmeUpArrayOfInt(int[] digits) {
+        return Arrays.copyOf(digits, digits.length + 1);
     }
+
 }
