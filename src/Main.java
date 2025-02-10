1c6148391b53055ac71e7e7e1b04d0336797f3f4
sidry-s-tavern <isgudoshnikov@gmail.com>
2025-02-10 17:36:44 +0400

    67. Add Binary. init commit

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        PlusOne plusOne = new PlusOne();
-        UsefullMethods.printIntArray(plusOne.plusOne(new int[]{4,9,9,9}));
+        AddBinary addBinary = new AddBinary();
+
     }
 }
