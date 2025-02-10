f14e1df5472ecbab1c6edda8c113f56ab4d7c759
sidry-s-tavern <isgudoshnikov@gmail.com>
2025-02-10 18:19:44 +0400

    67. Add Binary. 194 / 296. some wrong

--- src/AddBinary.java
+++ src/AddBinary.java
@@ -1,2 +1,28 @@
+//a (2) + b (2) = sum(2)
+// 11(2) + 1(2) = 100(2)
 public class AddBinary {
+    public String addBinary(String a, String b) {
+        Integer res = parseBin(a) + parseBin(b);
+        return Integer.toBinaryString(res);
+    }
+
+    public int parseBin(String a) {
+        int res = 0;
+        for (int i = 0; i < a.length(); i++) {
+            switch (a.charAt(i)) {
+                case '1': {
+                    res += (int) Math.pow(2, a.length() - i - 1);
+                    break;
+                }
+                case '0': {
+                    break;
+                }
+                default: {
+                    return -1;
+                }
+            }
+        }
+        return res;
+    }
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         AddBinary addBinary = new AddBinary();
-
+        System.out.println(addBinary.addBinary("1010","1011"));
     }
 }
