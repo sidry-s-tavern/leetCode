d9e88c125267612f4b46140f6cfc665960b24c1e
sidry-s-tavern <isgudoshnikov@gmail.com>
2025-02-10 15:10:51 +0400

    35. Search Insert Position. done

---
+++ src/SearchInsertPosition.java
@@ -0,0 +1,11 @@
+public class SearchInsertPosition {
+    public int searchInsert(int[] nums, int target) {
+        if (nums[0]==target) return 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] >= target) {
+                return i;
+            }
+        }
+        return nums.length;
+    }
+}
