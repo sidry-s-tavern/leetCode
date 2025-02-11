6bd0792da4387fbc467df2d6e1729cc5abfb9e30
sidry-s-tavern <isgudoshnikov@gmail.com>
2025-02-11 17:07:50 +0400

    67. Add Binary. 296 / 296. Done, but in 3ms

--- src/AddBinary.java
+++ src/AddBinary.java
@@ -2,21 +2,14 @@
 // 11(2) + 1(2) = 100(2)
 public class AddBinary {
     public String addBinary(String a, String b) {
-        return calcOfBin(a, b);
-    }
-
-    public String calcOfBin(String a, String b) {
         StringBuilder aBuilder = new StringBuilder(a);
         aBuilder.reverse();
         StringBuilder bBuilder = new StringBuilder(b);
         bBuilder.reverse();
         StringBuilder c = new StringBuilder();
-        System.out.println("a:" + a + " aBuild:" + aBuilder);
-        System.out.println("b:" + b + " bBuild:" + bBuilder);
         char mind = '0';
-        int x = 0;
         for (int i = 0; i < Math.min(a.length(), b.length()); i++) {
-            x = sumOfChars(aBuilder.charAt(i), bBuilder.charAt(i), mind);
+            int x = sumOfChars(aBuilder.charAt(i), bBuilder.charAt(i), mind);
             switch (x) {
                 case 0: {
                     c.append('0');
@@ -41,8 +34,8 @@ public class AddBinary {
                     return null;
             }
         }
-        for (int i = Math.min(a.length(), b.length()); i < Math.max(a.length(), b.length()) - 1; i++) {
-            x = (a.length() > b.length()) ? sumOfChars(aBuilder.charAt(i), '0', mind) : sumOfChars('0', bBuilder.charAt(i), mind);
+        for (int i = Math.min(a.length(), b.length()); i < Math.max(a.length(), b.length()); i++) {
+            int x = (a.length() > b.length()) ? sumOfChars(aBuilder.charAt(i), '0', mind) : sumOfChars('0', bBuilder.charAt(i), mind);
             switch (x) {
                 case 0: {
                     c.append('0');
