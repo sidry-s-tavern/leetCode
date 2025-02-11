18b596d3295a8287fb62692257004d747cb9973c
sidry-s-tavern <isgudoshnikov@gmail.com>
2025-02-11 17:05:11 +0400

    67. Add Binary. switch to StringBuilder and reverse

--- src/AddBinary.java
+++ src/AddBinary.java
@@ -2,27 +2,76 @@
 // 11(2) + 1(2) = 100(2)
 public class AddBinary {
     public String addBinary(String a, String b) {
-        Integer res = parseBin(a) + parseBin(b);
-        return Integer.toBinaryString(res);
+        return calcOfBin(a, b);
     }
 
-    public int parseBin(String a) {
-        int res = 0;
-        for (int i = 0; i < a.length(); i++) {
-            switch (a.charAt(i)) {
-                case '1': {
-                    res += (int) Math.pow(2, a.length() - i - 1);
+    public String calcOfBin(String a, String b) {
+        StringBuilder aBuilder = new StringBuilder(a);
+        aBuilder.reverse();
+        StringBuilder bBuilder = new StringBuilder(b);
+        bBuilder.reverse();
+        StringBuilder c = new StringBuilder();
+        System.out.println("a:" + a + " aBuild:" + aBuilder);
+        System.out.println("b:" + b + " bBuild:" + bBuilder);
+        char mind = '0';
+        int x = 0;
+        for (int i = 0; i < Math.min(a.length(), b.length()); i++) {
+            x = sumOfChars(aBuilder.charAt(i), bBuilder.charAt(i), mind);
+            switch (x) {
+                case 0: {
+                    c.append('0');
                     break;
                 }
-                case '0': {
+                case 1: {
+                    c.append('1');
+                    mind = '0';
                     break;
                 }
-                default: {
-                    return -1;
+                case 2: {
+                    c.append('0');
+                    mind = '1';
+                    break;
                 }
+                case 3: {
+                    c.append('1');
+                    mind = '1';
+                    break;
+                }
+                default:
+                    return null;
             }
         }
-        return res;
+        for (int i = Math.min(a.length(), b.length()); i < Math.max(a.length(), b.length()) - 1; i++) {
+            x = (a.length() > b.length()) ? sumOfChars(aBuilder.charAt(i), '0', mind) : sumOfChars('0', bBuilder.charAt(i), mind);
+            switch (x) {
+                case 0: {
+                    c.append('0');
+                    break;
+                }
+                case 1: {
+                    c.append('1');
+                    mind = '0';
+                    break;
+                }
+                case 2: {
+                    c.append('0');
+                    mind = '1';
+                    break;
+                }
+                case 3: {
+                    c.append('1');
+                    mind = '1';
+                    break;
+                }
+                default:
+                    return null;
+            }
+        }
+        if (mind == '1') c.append('1');
+        return c.reverse().toString();
     }
 
+    public int sumOfChars(char c1, char c2, char c3) {
+        return Integer.parseInt(String.valueOf(c1)) + Integer.parseInt(String.valueOf(c2)) + Integer.parseInt(String.valueOf(c3));
+    }
 }
