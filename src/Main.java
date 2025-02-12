a125525b0d01adfaf6ed9ab94a2993c9212df1c8
sidry-s-tavern <isgudoshnikov@gmail.com>
2025-02-12 17:12:40 +0400

    70. Climbing Stairs. done with fori and loop

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,10 @@
+import java.sql.Time;
+import java.time.LocalTime;
+
 public class Main {
     public static void main(String[] args) {
         ClimbingStairs climbingStairs = new ClimbingStairs();
-        System.out.println("result:"+climbingStairs.climbStairs(5));
+        System.out.println("result:" + climbingStairs.climbStairs(45));
+        System.out.println("result:" + climbingStairs.climbStairsLoop(45));
     }
 }
