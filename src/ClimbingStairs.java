893e36d955a55702121dc8ce70e5d880564884ea
sidry-s-tavern <isgudoshnikov@gmail.com>
2025-02-12 16:53:42 +0400

    70. Climbing Stairs. done with fori

--- src/ClimbingStairs.java
+++ src/ClimbingStairs.java
@@ -1,6 +1,15 @@
 public class ClimbingStairs {
     public int climbStairs(int n) {
-
-        return -1;
+        if (n==0) return 0;
+        if (n==1) return 1;
+        if (n==2) return 2;
+        int [] stairs = new int [n+1];
+        stairs[0]=0;
+        stairs[1]=1;
+        stairs[2]=2;
+        for (int i = 3; i < stairs.length; i++) {
+            stairs[i] = stairs[i-1]+stairs[i-2];
+        }
+        return stairs[stairs.length-1];
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-
+        ClimbingStairs climbingStairs = new ClimbingStairs();
+        System.out.println("result:"+climbingStairs.climbStairs(5));
     }
 }
