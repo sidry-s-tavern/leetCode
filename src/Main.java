b315d9bea975c960d6cd8bf2b8fd0ea3535a9a93
sidry-s-tavern <isgudoshnikov@gmail.com>
2025-10-16 12:49:38 +0400

    88. Merge Sorted Array. some troubles

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,6 @@
 public class Main {
     public static void main(String[] args) {
         MergeTwoSortedLists mergeTwoSortedLists = new MergeTwoSortedLists();
-        mergeTwoSortedLists.mergeTwoLists(new ListNode(0),new ListNode(0));
+        mergeTwoSortedLists.mergeTwoLists(new ListNode(3),new ListNode(4));
     }
 }
--- src/MergeTwoSortedLists.java
+++ src/MergeTwoSortedLists.java
@@ -17,16 +17,46 @@ class ListNode {
 
 public class MergeTwoSortedLists {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode list11 = new ListNode(1);
+        ListNode list12 = new ListNode(2);
+        ListNode list13 = new ListNode(4);
+        list11.next = list12;
+        list12.next = list13;
+//        list1 = [1,2,4], list2 = [1,3,4]
+        ListNode list21 = new ListNode(1);
+        ListNode list22 = new ListNode(3);
+        ListNode list23 = new ListNode(4);
+        list21.next = list22;
+        list22.next = list23;
+        printList(list11);
+        System.out.println();
+        printList(list21);
+
         return new ListNode();
     }
 
-    public int foo(ListNode list)
-    {
-        ListNode curr = list.next;
-        while (!list.next.equals(null))
-        {
-            return list.val;
+    public void foo(ListNode list1, ListNode list2) {
+        ListNode curr1 = list1;
+        ListNode curr2 = list2;
+
+        ListNode res = new ListNode();
+        while ((curr2.next != null) && (curr1.next != null)) {
+            if (curr1.val > curr2.val) {
+                res.val = curr1.val;
+                curr1 = curr1.next;
+            } else {
+                res.val = curr2.val;
+                curr2 = curr2.next;
+            }
+            res.next = new ListNode();
         }
-        return list.val;
+
+    }
+
+    public void printList(ListNode list) {
+        if (list.next != null) {
+            printList(list.next);
+        }
+        System.out.print(list.val + " ");
     }
 }
