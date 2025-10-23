3de25dd314c0bbfea1e6c295d07946fd7ab0a3d9
sidry-s-tavern <isgudoshnikov@gmail.com>
2025-10-23 15:43:22 +0400

    1331. Rank Transform of an Array First realise

--- src/Main.java
+++ src/Main.java
@@ -4,9 +4,11 @@ public class Main {
 //        int[] nums = {40, 10, 20, 30};
 //        int[] nums = {100,100,100};
         int[] nums = {37,12,28,9,100,56,80,5,12};
+//        int[] nums = {-4};
 //    [100,100,100]
 //    [37,12,28,9,100,56,80,5,12]
 
         UsefullMethods.printIntArray(rankTransformOfAnArray.arrayRankTransform(nums));
+        rankTransformOfAnArray.foo(nums);
     }
 }
--- src/RankTransformOfAnArray.java
+++ src/RankTransformOfAnArray.java
@@ -1,4 +1,5 @@
-import java.util.Arrays;
+import java.util.*;
+import java.util.stream.Collectors;
 
 public class RankTransformOfAnArray {
 //    [40,10,20,30]
@@ -11,9 +12,9 @@ public class RankTransformOfAnArray {
 
     public int[] dumpRank(int[] array) {
         for (int i = 0; i < array.length; i++) {
-            int k = i+1;
-            if(Arrays.stream(array).filter(e -> e > k).min().isPresent())
-                replaseDigitToMin(array,Arrays.stream(array).filter(e -> e > k).min().getAsInt(),i+1,i);
+            int k = i + 1;
+            if (Arrays.stream(array).filter(e -> e > k).min().isPresent())
+                replaseDigitToMin(array, Arrays.stream(array).filter(e -> e > k).min().getAsInt(), i + 1, i);
         }
         return array;
     }
@@ -27,5 +28,35 @@ public class RankTransformOfAnArray {
                 }
             }
         }
+    }
+
+    public void foo(int[] digits) {
+        int[][] digitsSqr = new int[digits.length][1];
+        for (int i = 0; i < digits.length; i++) {
+            digitsSqr[i][0] = digits[i];
+        }
+        if (Arrays.stream(digits).min().isPresent()) {
+            for (int i = 0; i < digitsSqr.length; i++) {
+                if (digitsSqr[i][0] == Arrays.stream(digits).min().getAsInt()) {
+                    digitsSqr[i][1] = 1;
+                }
+            }
+        }
+    }
+
+    public int[] workArray(int[] digits, int[] except) {
+        List<Integer> list = new ArrayList<Integer>();
+        for (int i = 0; i < digits.length; i++) {
+            boolean isExcepted = false;
+            for (int j = 0; j < except.length; j++) {
+                if(digits[i]==except[j]) isExcepted = true;
+            }
+            if (!isExcepted) list.add(digits[i]);
+        }
+        int[] res = new int[list.size()];
+        for (int i = 0; i < list.size(); i++) {
+            res[i] = list.get(i);
+        }
+        return new int[0];
     }
 }
